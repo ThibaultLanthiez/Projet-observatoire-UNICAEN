a3fa426780381f0356c5b1c52a969d3ab197346a
ThibaultLanthiez <65160656+ThibaultLanthiez@users.noreply.github.com>
2021-01-02 16:04:15 +0100

    Create naf.py

---
+++ Codes/Files to SQL/naf.py
@@ -0,0 +1,17 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('data.db')
+c= conn.cursor()
+
+with open('naf.csv', newline='') as csvfile:
+    csvfile.readline()
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='"')
+    for row in spamreader :    
+        requete = "INSERT INTO naf VALUES(\""
+        requete += row[0].replace(".","")+"\",\""+row[4]+"\")"
+        c.execute(requete)
+        
+conn.commit()
+conn.close()
+
